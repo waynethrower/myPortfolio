ecc61698697c3149f2b0a5cf66fd34a2d78c4ec2
waynethrower <wayne.thrower@hotmail.co.uk>
2022-04-05 18:15:16 +0100

    Media Query values needed adjusting

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -13,7 +13,7 @@ function Contact() {
         <div className="col__2">
           <div className="contact__box">
             <div className="contact__meta">
-              <h1 className="hire__text">Hire Me.</h1>
+              <h1 className="hire__text">Hire Me</h1>
               <p className="hire__text white">
                 I am available for work. Connect with me via email:
               </p>
