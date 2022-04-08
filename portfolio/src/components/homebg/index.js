36b1481109248cccedfcdefb0b7398154642468f
waynethrower <wayne.thrower@hotmail.co.uk>
2022-04-08 13:51:03 +0100

    corrected the size of the icons so they adjust according to the screen size

--- portfolio/src/components/homebg/index.js
+++ portfolio/src/components/homebg/index.js
@@ -4,7 +4,11 @@ import React from "react"
 import "./HomeBG.css"
 
 //Assets
+// import c from "../../img/icons/c.png"
+import csharp from "../../img/icons/csharp.png"
+// import cpp from "../../img/icons/c++.png"
 import css from "../../img/icons/css3.png"
+import cypress from "../../img/icons/cypress.png"
 import express from "../../img/icons/express.png"
 import figma from "../../img/icons/figma.png"
 import git from "../../img/icons/Github.png"
@@ -13,17 +17,19 @@ import html from "../../img/icons/html5.png"
 import java from "../../img/icons/java.png"
 import js from "../../img/icons/javascript-logo.svg"
 import jest from "../../img/icons/jest.png"
+// import mongo from "../../img/icons/mongo.png"
 // import mongodb from "../../img/icons/mongodb.png"
 import mysql from "../../img/icons/mySQL.png"
 import netlify from "../../img/icons/netlify.png"
 import nextjs from "../../img/icons/nextjs.png"
 import nodeJS from "../../img/icons/nodeJS.png"
+import npm from "../../img/icons/npm.png"
 import postgresql from "../../img/icons/postgresql.png"
 import react from "../../img/icons/ReactIcon.png"
 // import sass from "../../img/icons/sass.png"
 import slack from "../../img/icons/slack.png"
 import soc from "../../img/icons/SOC.png"
-// import typescript from "../../img/icons/typescript.png"
+import typescript from "../../img/icons/typescript.png"
 import uon from "../../img/icons/UON.png"
 import vstudio from "../../img/icons/visualstudio.png"
 import vscode from "../../img/icons/vscode.png"
@@ -33,66 +39,78 @@ function HomeBG() {
     <>
       <div className="square">
         <div>
-          <img src={css} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={css} alt=""></img>
         </div>
         <div>
-          <img src={git} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={git} alt=""></img>
         </div>
         <div>
-          <img src={express} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={express} alt=""></img>
         </div>
         <div>
-          <img src={figma} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={figma} alt=""></img>
         </div>
         <div>
-          <img src={nodeJS} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={nodeJS} alt=""></img>
         </div>
         <div>
-          <img src={html} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={html} alt=""></img>
         </div>
         <div>
-          <img src={js} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={js} alt=""></img>
         </div>
         <div>
-          <img src={soc} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={soc} alt=""></img>
         </div>
         <div>
-          <img src={vscode} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={vscode} alt=""></img>
         </div>
         <div>
-          <img src={mysql} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={mysql} alt=""></img>
+        </div>
+        <div>
+          <img className="responsive" src={npm} alt=""></img>
+        </div>
+        <div>
+          <img className="responsive" src={csharp} alt=""></img>
         </div>
       </div>
       <div className="square square1">
         <div>
-          <img src={heroku} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={heroku} alt=""></img>
         </div>
         <div>
-          <img src={jest} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={jest} alt=""></img>
         </div>
         <div>
-          <img src={postgresql} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={postgresql} alt=""></img>
         </div>
         <div>
-          <img src={netlify} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={netlify} alt=""></img>
         </div>
         <div>
-          <img src={nextjs} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={nextjs} alt=""></img>
         </div>
         <div>
-          <img src={slack} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={slack} alt=""></img>
         </div>
         <div>
-          <img src={react} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={react} alt=""></img>
         </div>
         <div>
-          <img src={uon} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={uon} alt=""></img>
         </div>
         <div>
-          <img src={vstudio} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={vstudio} alt=""></img>
         </div>
         <div>
-          <img src={java} alt="" height="100px" width="100px"></img>
+          <img className="responsive" src={java} alt=""></img>
+        </div>
+        <div>
+          <img className="responsive" src={cypress} alt=""></img>
+        </div>
+        <div>
+          <img className="responsive" src={typescript} alt=""></img>
         </div>
       </div>
     </>
